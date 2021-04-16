e44f4aa89cd9613ef4723e07e4378639cd14d3f0
GabrielaGonzales <80542230+GabrielaGonzales@users.noreply.github.com>
2021-04-15 21:36:35 -0400

    Delete PracticaUnidad4(POO)VSCode directory

--- PracticaUnidad4(POO)VSCode/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        AsistentePersona asistente = new AsistentePersona("Gabriela");
-        System.out.println(asistente.agregarNombre());
-        asistente.cambiarNombre("Santi");
-        System.out.println(asistente.agregarNombre());
-    }
-}
